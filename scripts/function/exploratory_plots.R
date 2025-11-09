50f2960da118321f3191af1b7fd1031ef48021e1
Markella Moraitou <markella.mor@gmail.com>
2025-11-09 12:57:44 +0100

    separated script for plotting and analysing non KEGG annotations

--- scripts/function/exploratory_plots.R
+++ scripts/function/exploratory_plots.R
@@ -19,6 +19,7 @@ library(ggplot2)
 library(RColorBrewer)
 library(ggnewscale)
 library(scales)
+library(ggExtra)
 
 #### VARIABLES AND WORKING DIRECTORY ####
 
@@ -70,48 +71,54 @@ phy_gene_f_clr <- phy_gene_f_clr %>%
 
 # Species traits to use as constraints
 species_traits <- c("Artiodactyla", "Perissodactyla", "Primates", "Rodentia",
-                    "ruminant", "marine", "cf", "cp")
+                    "ruminant", "marine", "Fruit", "Animal")
 
 # Ordinate using all data
 ord <- ord_calc(phy_gene_f_clr, constraints = species_traits, method = "RDA")
+
+# Select variables and check for collinearity
+ord_step <- step(ord@ord, scope = formula(ord@ord), test = "perm")
+vif.cca(ord_step)
 
 # Scree plot
 p <- ord %>% ord_get() %>% plot_scree() + custom_theme() +
             xlim(c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10"))
 
-ggsave(file.path(subdir, "screeplot_genes.png"), p, width=8, height=6)
+ggsave(file.path(subdir, "screeplot_genes.png"), p, width=3, height=3)
 
 # Color by order
 
 #### Get shape scales for plotting ####
 diet_shape_scale <- c("Animalivore" = 8, "Omnivore" = 9 , "Frugivore" = 2, "Herbivore" = 16)
 
-p <- ord_plot(ord, colour="Order_grouped", shape="diet.general", alpha = 0.5, size = "Total_abundance") +
+p <- ord_plot(ord, colour="Order_grouped", shape="diet.general", alpha = 0.5) +
   custom_theme() +
   scale_shape_manual(values=diet_shape_scale, name = "Estimated diet") +
   scale_color_manual(values=order_palette, name = "Order") +
-  scale_size(name = "# Features") +
   geom_phylopic(data = centroids(ord@ord, phy_gene_f), aes(colour = Order_grouped), uuid = centroids(ord@ord, phy_gene_f)$uid, width = 0.3, fill = "transparent") +
   theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8)) +
-  guides(colour = guide_legend(ncol = 1, size = 1, byrow = TRUE))
+  guides(shape = guide_legend(ncol = 2), colour = guide_legend(ncol = 2))
+  
+p <- ggMarginal(p, type="violin", groupColour = TRUE, groupFill = TRUE, size=5)
 
-ggsave(p, filename = file.path(subdir, "gene_ordination_order.png"), width=8, height=10)
+ggsave(p, filename = file.path(subdir, "gene_ordination_order.png"), width=6, height=6)
 
 # Color by diet
 
 #### Get shape scales for plotting ####
 order_shape_scale <- c("Carnivora" = 4, "Primates" = 19, "Artiodactyla" = 5, "Perissodactyla" = 2, "Rodentia" = 1, "Rest" = 12, "Proboscidea_Sirenia" = 12)
 
-p <- ord_plot(ord, colour="diet.general", shape="Order_grouped", alpha = 0.5, size = "Total_abundance") +
+p <- ord_plot(ord, colour="diet.general", shape="Order_grouped", alpha = 0.5) +
   custom_theme() +
   scale_shape_manual(values=order_shape_scale, name = "Order") +
   scale_color_manual(values=diet_palette, name = "Estimated diet") +
-  scale_size(name = "# Features") +
   geom_phylopic(data = centroids(ord@ord, phy_gene_f), aes(colour = diet.general), uuid = centroids(ord@ord, phy_gene_f)$uid, width = 0.3, fill = "transparent") +
   theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8)) +
-  guides(colour = guide_legend(ncol = 1, size = 1, byrow = TRUE))
+  guides(shape = guide_legend(ncol = 2), colour = guide_legend(ncol = 2))
 
-ggsave(p, filename = file.path(subdir, "gene_ordination_diet.png"), width=8, height=10)
+p <- ggMarginal(p, type="violin", groupColour = TRUE, groupFill = TRUE, size=5)
+
+ggsave(p, filename = file.path(subdir, "gene_ordination_diet.png"), width=6, height=6)
 
 # Plot arrows
 # Get loading arrows coordinaties
@@ -144,59 +151,62 @@ arrow_colours["Other"] <- "grey90" # Set "Other" to grey
 p <- ggplot(data = arrows_filt) +
   geom_segment(aes(x = 0, y = 0, xend = RDA1, yend = RDA2, colour = category_grouped), linewidth = 0.5, alpha = 0.5) +
   scale_color_manual(values = arrow_colours, name = "Module") +
-  xlab("RDA1") + ylab("RDA2")
+  xlab("RDA1") + ylab("RDA2") +
+  theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8)) +
+  guides(colour = guide_legend(nrow = 2))
 
-ggsave(p, filename = file.path(subdir, "gene_ordination_arrows.png"), width=8, height=6)
+ggsave(p, filename = file.path(subdir, "gene_ordination_arrows.png"), width=6, height=6)
 
 #### PATH ABUNDANCE (CLR) ####
 
-phy_pathway_clr <- phy_pathway_clr %>% subset_samples(Order %in% c("Artiodactyla", "Carnivora", "Perissodactyla", "Primates"))
-
 phy_pathway_clr <- phy_pathway_clr %>% 
         ps_mutate(Artiodactyla = (Order == "Artiodactyla"),
-                  #Carnivora = (Order == "Carnivora"),
+                  Carnivora = (Order == "Carnivora"),
                   Perissodactyla = (Order == "Perissodactyla"),
                   Primates = (Order == "Primates"),
-                  #Rodentia = (Order == "Rodentia"),
+                  Rodentia = (Order == "Rodentia"),
                   ruminant = (digestion == "Ruminant"),
                   marine = (habitat.general == "Marine"),
                   animalivore = (diet.general == "Animalivore"))
 
-species_traits <- c("Artiodactyla", "Perissodactyla", "Primates",
-                    "ruminant", "marine", "cf", "cp")
-
 # Ordinate using all data
 ord <- ord_calc(phy_pathway_clr, constraints = species_traits, method = "RDA")
+
+# Select variables and check for collinearity
+ord_step <- step(ord@ord, scope = formula(ord@ord), test = "perm")
+vif.cca(ord_step)
 
 # Scree plot
 p <- ord %>% ord_get() %>% plot_scree() + custom_theme() +
             xlim(c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10"))
 
-ggsave(file.path(subdir, "screeplot_pathways.png"), p, width=8, height=6)
+ggsave(file.path(subdir, "screeplot_pathways.png"), p, width=3, height=3)
 
 # Color by order
-p <- ord_plot(ord, colour="Order_grouped", shape="diet.general", alpha = 0.5, size = "Total_abundance") +
+p <- ord_plot(ord, colour="Order_grouped", shape="diet.general", alpha = 0.5) +
   custom_theme() +
   scale_shape_manual(values=diet_shape_scale, name = "Estimated diet") +
   scale_color_manual(values=order_palette, name = "Order") +
-  scale_size(name = "# Features") +
-  geom_phylopic(data = centroids(ord@ord, phy_pathway_clr), aes(colour = Order_grouped), uuid = centroids(ord@ord, phy_pathway_clr)$uid, width = 0.3, fill = "transparent") +
+  geom_phylopic(data = centroids(ord@ord, phy_pathway_clr), aes(colour = Order_grouped), uuid = centroids(ord@ord, phy_pathway_clr)$uid, width = 0.2, fill = "transparent") +
   theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8)) +
-  guides(colour = guide_legend(ncol = 1, size = 1, byrow = TRUE))
+  guides(shape = guide_legend(ncol = 2), colour = guide_legend(ncol = 2))
 
-ggsave(p, filename = file.path(subdir, "pathway_ordination_order.png"), width=8, height=10)
+p <- ggMarginal(p, type="violin", groupColour = TRUE, groupFill = TRUE, size=5)
+
+ggsave(p, filename = file.path(subdir, "pathway_ordination_order.png"), width=6, height=6)
 
 # Color by diet
-p <- ord_plot(ord, colour="diet.general", shape="Order_grouped", alpha = 0.5, size = "Total_abundance") +
+p <- ord_plot(ord, colour="diet.general", shape="Order_grouped", alpha = 0.5) +
   custom_theme() +
   scale_shape_manual(values=order_shape_scale, name = "Order") +
   scale_color_manual(values=diet_palette, name = "Estimated diet") +
-  scale_size(name = "# Features") +
-  geom_phylopic(data = centroids(ord@ord, phy_pathway_clr), aes(colour = diet.general), uuid = centroids(ord@ord, phy_pathway_clr)$uid, width = 0.3, fill = "transparent") +
+  geom_phylopic(data = centroids(ord@ord, phy_pathway_clr), aes(colour = diet.general), uuid = centroids(ord@ord, phy_pathway_clr)$uid, width = 0.2, fill = "transparent") +
   theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8)) +
-  guides(colour = guide_legend(ncol = 1, size = 1, byrow = TRUE))
+  guides(shape = guide_legend(ncol = 2), colour = guide_legend(ncol = 2))
 
-ggsave(p, filename = file.path(subdir, "pathway_ordination_diet.png"), width=8, height=10)
+p <- ggMarginal(p, type="violin", groupColour = TRUE, groupFill = TRUE, size=5)
+
+ggsave(p, filename = file.path(subdir, "pathway_ordination_diet.png"), width=6, height=6)
 
 # Plot arrows
 # Get loading arrows coordinaties
@@ -207,7 +217,7 @@ arrows$name <- as.character(phy_pathway_clr@tax_table[match(rownames(arrows), ro
 arrows$category <- as.character(phy_pathway_clr@tax_table[match(rownames(arrows), rownames(phy_pathway_clr@tax_table)), "path_class"])
 arrows$category <- str_remove(arrows$category, ".*; ")
 
-arrows$to_plot <- (rownames(arrows) %in% head(rownames(arrows), 25))
+arrows$to_plot <- (rownames(arrows) %in% head(rownames(arrows), nrow(arrows)))
 
 # Save
 write.csv(rownames_to_column(arrows, "gene"), file.path(subdir, "gene_ordination_arrows.txt"), quote = FALSE, row.names = FALSE)
@@ -217,7 +227,7 @@ arrows_filt <- arrows %>% filter(to_plot) %>%
               select(contains(c("1", "2")), name, category)
 
 # Group uncommon categories
-common_categories <- table(arrows_filt$category) %>% sort(decreasing = TRUE) %>% head(6) %>% names
+common_categories <- table(arrows_filt$category) %>% sort(decreasing = TRUE) %>% head(8) %>% names
 
 arrows_filt <- arrows_filt %>%
     mutate(category_grouped = factor(case_when(category %in% common_categories ~ category,
@@ -230,109 +240,12 @@ arrow_colours["Other"] <- "grey90" # Set "Other" to grey
 
 p <- ggplot(data = arrows_filt) +
   geom_segment(aes(x = 0, y = 0, xend = RDA1, yend = RDA2, colour = category_grouped), linewidth = 0.5, alpha = 0.5) +
-  geom_text(aes(x = RDA1, y = RDA2, label = name, hjust = ifelse(RDA1 < 0, 1, 0)), size = 1.5, vjust = 1) +
+  #geom_text(aes(x = RDA1, y = RDA2, label = name, hjust = ifelse(RDA1 < 0, 1, 0)), size = 1.5, vjust = 1) +
   scale_color_manual(values = arrow_colours, name = "Pathway class") +
   xlim(c(min(arrows_filt$RDA1)*2, max(arrows_filt$RDA1)*2)) +
   xlab("RDA1") + ylab("RDA2") +
-  theme(legend.position = "bottom", legend.direction = "vertical", legend.text = element_text(size = 8))
+  theme(legend.position = "bottom", legend.direction = "vertical",
+        legend.text = element_text(size = 7), legend.title = element_text(size = 10)) +
+  guides(colour = guide_legend(ncol = 2))
 
-ggsave(p, filename = file.path(subdir, "pathway_ordination_arrows.png"), width=8, height=8)
-
-#############################
-#### NON KEGG ANNOTATIOS ####
-#############################
-
-# Plot peptidase diversity
-
-phy_merops <- phy_gene_f %>% subset_taxa(database == "MEROPS")
-
-phy_merops_comp <- phy_gene_f %>% transform("compositional") %>% subset_taxa(database == "MEROPS")
-
-merops_richness <- 
-    estimate_richness(phy_merops, measures = "Observed") %>%
-    rownames_to_column("Sample") %>%
-    left_join(select(data.frame(sample_data(phy_merops)), Common.name, cp, diet.general, Order_grouped) %>%
-                as.data.frame() %>% rownames_to_column("Sample"))
-
-p <-
-    ggplot(merops_richness, aes(x = cp/100, y = Observed, colour = diet.general)) +
-    geom_point(size = 3, alpha = 0.7) +
-    geom_smooth(method = "lm", se = FALSE, linewidth = 0.5, alpha = 0.5, colour = "black") +
-    scale_colour_manual(values = diet_palette, name = "Diet") +
-    labs(x = "crude protein", y = "Peptidase richness (Observed)") +
-    scale_x_continuous(labels = percent) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggsave(file.path(subdir, "peptidase_richness.png"), width=8, height=6)
-
-# Plot peptidase abundance
-
-merops_abundance <- 
-    data.frame(phy_merops_comp@otu_table) %>% rownames_to_column("OTU") %>%
-    pivot_longer(cols = -OTU, names_to = "Sample", values_to = "Abundance") %>%
-    left_join(sample_data(phy_merops_comp) %>% data.frame() %>% rownames_to_column("Sample") %>%
-                select(Sample, Common.name, cp, diet.general, Order_grouped)) %>%
-    group_by(Common.name, cp, diet.general, Order_grouped) %>%
-    # Get mean and quartile abundance of peptidases per species
-    summarise(Abundance = mean(Abundance, na.rm = TRUE),
-              q1 = quantile(Abundance, probs = 0.25),
-              q3 = quantile(Abundance, probs = 0.75), .groups = "drop")
-
-p <-
-    ggplot(merops_abundance, aes(x = cp/100, y = Abundance, colour = diet.general)) +
-    geom_point(size = 3, alpha = 0.7) +
-    geom_smooth(method = "lm", se = FALSE, linewidth = 0.5, alpha = 0.5, colour = "black") +
-    scale_colour_manual(values = diet_palette, name = "Diet") +
-    labs(x = "crude protein", y = "Peptidase relative abundance") +
-    scale_x_continuous(labels = percent) +
-    scale_y_continuous(labels = percent) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggsave(file.path(subdir, "peptidase_abundance.png"), width=8, height=6)
-
-# Plot CAZy enzyme diversity
-
-phy_cazy <- phy_gene_f %>% subset_taxa(database == "CAZY")
-
-phy_cazy_comp <- phy_gene_f %>% transform("compositional") %>% subset_taxa(database == "CAZY")
-
-cazy_richness <- 
-    estimate_richness(phy_cazy, 1000, measures = "Observed") %>%
-    rownames_to_column("Sample") %>%
-    left_join(select(data.frame(sample_data(phy_cazy)), Common.name, cp, diet.general, Order_grouped) %>%
-                as.data.frame() %>% rownames_to_column("Sample"))
-
-p <-
-    ggplot(cazy_richness, aes(x = cp/100, y = Observed, colour = diet.general)) +
-    geom_point(size = 3, alpha = 0.7) +
-    geom_smooth(method = "lm", se = FALSE, linewidth = 0.5, alpha = 0.5, colour = "black") +
-    scale_colour_manual(values = diet_palette, name = "Diet") +
-    labs(x = "crude protein", y = "CAZy richness (Observed)") +
-    scale_x_continuous(labels = percent) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggsave(file.path(subdir, "cazy_richness.png"), width=8, height=6)
-
-# Plot CAZy abundance
-cazy_abundance <- 
-    data.frame(phy_cazy_comp@otu_table) %>% rownames_to_column("OTU") %>%
-    pivot_longer(cols = -OTU, names_to = "Sample", values_to = "Abundance") %>%
-    left_join(sample_data(phy_cazy_comp) %>% data.frame() %>% rownames_to_column("Sample") %>%
-                select(Sample, Common.name, cp, diet.general, Order_grouped)) %>%
-    group_by(Common.name, cp, diet.general, Order_grouped) %>%
-    # Get mean and quartile abundance of CAZy enzymes per species
-    summarise(Abundance = mean(Abundance, na.rm = TRUE),
-              q1 = quantile(Abundance, probs = 0.25),
-              q3 = quantile(Abundance, probs = 0.75), .groups = "drop")
-
-p <-
-    ggplot(cazy_abundance, aes(x = cp/100, y = Abundance, colour = diet.general)) +
-    geom_point(size = 3, alpha = 0.7) +
-    geom_smooth(method = "lm", se = FALSE, linewidth = 0.5, alpha = 0.5, colour = "black") +
-    scale_colour_manual(values = diet_palette, name = "Diet") +
-    labs(x = "crude protein", y = "CAZy relative abundance") +
-    scale_x_continuous(labels = percent) +
-    scale_y_continuous(labels = percent) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggsave(file.path(subdir, "cazy_abundance.png"), width=8, height=6)
+ggsave(p, filename = file.path(subdir, "pathway_ordination_arrows.png"), width=6, height=5)
---
+++ scripts/function/nonkegg.R
@@ -0,0 +1,160 @@
+#### PLOTS AND MODELS FOR NON KEGG ANNOTATIONS (MEROPS AND CAZY) ####
+
+################
+#### SET UP ####
+################
+
+#### LOAD PACKAGES ####
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(stringr)
+library(phyloseq)
+library(microbiome)
+library(ggplot2)
+library(lme4)
+
+#### VARIABLES AND WORKING DIRECTORY ####
+
+# Directory and file paths paths
+indir <- normalizePath(file.path("..", "..", "input")) # Directory with phyloseq output and sample metadata 
+datadir <- normalizePath(file.path("..", "..", "output", "function", "data"))
+subdir <- normalizePath(file.path("..", "..", "output", "function", "non_kegg")) # subdirectory for the output of this script
+
+dir.create(subdir, recursive = TRUE, showWarnings = FALSE)
+
+## Set up for plotting
+source(file.path("..", "plot_setup.R"))
+plot_setup(file.path("..", "..", "input", "palettes"))
+theme_set(custom_theme())
+
+#######################
+#####  LOAD INPUT #####
+#######################
+
+# Phyloseq objects
+phy_gene_f <- readRDS(file.path(datadir, "phy_gene_f.RDS"))
+phy_gene_f_clr <- readRDS(file.path(datadir, "phy_gene_f_clr.RDS"))
+
+#############################
+#### NON KEGG ANNOTATIOS ####
+#############################
+
+set.seed(123)
+
+# Plot peptidase diversity
+# Rarefy even depth before calculating richness
+phy_merops <- phy_gene_f %>% rarefy_even_depth(2e+07) %>% subset_taxa(database == "MEROPS")
+
+phy_merops_comp <- phy_gene_f %>% transform("compositional") %>% subset_taxa(database == "MEROPS")
+
+merops_richness <- 
+    estimate_richness(phy_merops, measures = "Observed") %>%
+    rownames_to_column("Sample") %>%
+    left_join(select(data.frame(sample_data(phy_merops)), Common.name, Animal, Fruit, digestion, diet.general, Order, Order_grouped, habitat.general) %>%
+                as.data.frame() %>% rownames_to_column("Sample")) %>%
+    mutate(ruminant = (digestion == "Ruminant"),
+           marine = (habitat.general == "Marine"))
+
+write.csv(merops_richness, file.path(subdir, "peptidase_richness.csv"), row.names = FALSE)
+
+model <- glmer(Observed ~ Animal + Fruit + ruminant + marine + (1|Common.name), data = merops_richness, family = poisson)
+res <- summary(model)$coefficients
+
+label <- data.frame(res) %>% filter(Pr...z.. < 0.05) %>%
+    rownames_to_column("Variable") %>%
+    filter(Variable != "(Intercept)") %>%
+    mutate(label = paste0(Variable, " coef = ", signif(Estimate, 2), ", p = ", signif(Pr...z.., 2))) %>%
+    pull(label) %>%
+    paste(collapse = "\n")
+
+p <-
+    ggplot(merops_richness, aes(x = as.factor(Animal), y = Observed, fill = Order)) +
+    geom_boxplot() +
+    scale_fill_manual(values = order_palette, name = "Taxonomic order") +
+    labs(x = "Animal % in diet", y = "Peptidase richness (Observed)") +
+    annotate("text", x = Inf, y = Inf,
+              label = label, hjust = 1.1, vjust = 1.1, size = 3) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+
+ggsave(file.path(subdir, "peptidase_richness.png"), width=5, height=4)
+
+# Plot peptidase abundance
+
+merops_abundance <- 
+    data.frame(phy_merops_comp@otu_table) %>% rownames_to_column("OTU") %>%
+    pivot_longer(cols = -OTU, names_to = "Sample", values_to = "Abundance") %>%
+    left_join(sample_data(phy_merops_comp) %>% data.frame() %>% rownames_to_column("Sample") %>%
+                select(Sample, Common.name, Animal, diet.general, Order, Order_grouped)) %>%
+    group_by(Common.name, Animal, diet.general, Order, Order_grouped) %>%
+    # Get mean and quartile abundance of peptidases per species
+    summarise(Abundance = mean(Abundance, na.rm = TRUE),
+              q1 = quantile(Abundance, probs = 0.25),
+              q3 = quantile(Abundance, probs = 0.75), .groups = "drop")
+
+p <-
+    ggplot(merops_abundance, aes(x = as.factor(Animal), y = Abundance, fill = Order)) +
+    geom_boxplot() +
+    scale_fill_manual(values = order_palette, name = "Taxonomic order") +
+    labs(x = "Animal % in diet", y = "Peptidase relative abundance") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+
+ggsave(file.path(subdir, "peptidase_abundance.png"), width=5, height=4)
+
+# Plot CAZy enzyme diversity
+
+phy_cazy <- phy_gene_f %>% rarefy_even_depth(2e+07) %>% subset_taxa(database == "CAZY")
+
+phy_cazy_comp <- phy_gene_f %>% transform("compositional") %>% subset_taxa(database == "CAZY")
+
+cazy_richness <- 
+    estimate_richness(phy_cazy, 1000, measures = "Observed") %>%
+    rownames_to_column("Sample") %>%
+    left_join(select(data.frame(sample_data(phy_merops)), Common.name, Animal, Fruit, digestion, diet.general, Order, Order_grouped, habitat.general) %>%
+                as.data.frame() %>% rownames_to_column("Sample")) %>%
+    mutate(ruminant = (digestion == "Ruminant"),
+           marine = (habitat.general == "Marine"))
+
+write.csv(cazy_richness, file.path(subdir, "cazy_richness.csv"), row.names = FALSE)
+
+model <- glmer(Observed ~ Animal + Fruit + ruminant + marine + (1|Common.name), data = cazy_richness, family = poisson)
+res <- summary(model)$coefficients
+
+label <- data.frame(res) %>% filter(Pr...z.. < 0.05) %>%
+    rownames_to_column("Variable") %>%
+    filter(Variable != "(Intercept)") %>%
+    mutate(label = paste0(Variable, " coef = ", signif(Estimate, 2), ", p = ", signif(Pr...z.., 2))) %>%
+    pull(label) %>%
+    paste(collapse = "\n")
+
+p <-
+    ggplot(cazy_richness, aes(x = as.factor(Animal), y = Observed, fill = Order)) +
+    geom_boxplot() +
+    scale_fill_manual(values = order_palette, name = "Taxonomic order") +
+    annotate("text", x = Inf, y = Inf,
+              label = label, hjust = 1.1, vjust = 1.1, size = 3) +
+    labs(x = "Animal % in diet", y = "CAZy richness (Observed)") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+
+ggsave(file.path(subdir, "cazy_richness.png"), width=5, height=4)
+
+# Plot CAZy abundance
+cazy_abundance <- 
+    data.frame(phy_cazy_comp@otu_table) %>% rownames_to_column("OTU") %>%
+    pivot_longer(cols = -OTU, names_to = "Sample", values_to = "Abundance") %>%
+    left_join(sample_data(phy_cazy_comp) %>% data.frame() %>% rownames_to_column("Sample") %>%
+                select(Sample, Common.name, Animal, diet.general, Order, Order_grouped)) %>%
+    group_by(Common.name, Animal, diet.general, Order, Order_grouped) %>%
+    # Get mean and quartile abundance of CAZy enzymes per species
+    summarise(Abundance = mean(Abundance, na.rm = TRUE),
+              q1 = quantile(Abundance, probs = 0.25),
+              q3 = quantile(Abundance, probs = 0.75), .groups = "drop")
+
+p <-
+    ggplot(cazy_abundance, aes(x = factor(Animal), y = Abundance, fill = Order)) +
+    geom_boxplot() +
+    scale_fill_manual(values = order_palette, name = "Taxonomic order") +
+    labs(x = "Animal % in diet", y = "CAZy relative abundance") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave(file.path(subdir, "cazy_abundance.png"), width=5, height=4)
