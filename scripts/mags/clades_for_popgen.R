e9820bb4d37eda81b8674eaef66e2a9dbabc9de7
Markella Moraitou <markella.mor@gmail.com>
2026-03-26 19:49:21 +0100

    Give refs names and get forward branches

--- scripts/mags/clades_for_popgen.R
+++ scripts/mags/clades_for_popgen.R
@@ -14,6 +14,7 @@ library(stringr)
 library(ape)
 library(phytools)
 library(tibble)
+library(rlang)
 
 #### VARIABLES AND WORKING DIRECTORY ####
 
@@ -35,13 +36,13 @@ setwd(subdir)
 #######################
 
 # MAG bacteria tree
-bac_tree <- read.tree(file = file.path(outdir, "bac_tree.tree"))
+bac_tree <- read.tree(file = file.path(outdir, "bac_tree_drep.tree"))
 
 # Entire bacteria tree
 bac_tree_full <- read.tree(file = file.path(indir, "gtdbtk.bac120.decorated_itol.tree"))
 
 # Bacterial MAG metadata
-bac_meta <- read.table(file.path(outdir, "bac_meta.tsv"), sep="\t", header=TRUE)
+bac_meta <- read.table(file.path(outdir, "bac_meta_drep.tsv"), sep="\t", header=TRUE)
 
 # Codiversifying clades
 bac_cod <- read.csv(file.path(outdir, "codiversification", "bac_cod_results.csv"), header=TRUE)
@@ -65,12 +66,30 @@ names(cod_tips) <- cod_nodes
 cod_tips_df <- enframe(cod_tips, name = "node", value = "tip") %>%
   unnest(cols = c(tip)) %>%
   # Get MAG file names
-  left_join(unique(select(bac_meta, c(label, bin, Completeness, Contamination))), by = c("tip" = "label"))
+  left_join(unique(select(bac_meta, c(label, bin, Completeness, Contamination, host_order))), by = c("tip" = "label"))
+
+#### Get info on reference genomes ####
+ref_info <- select(bac_meta, closest_genome_reference.1, species) %>% rename(accession = closest_genome_reference.1) %>%
+  rbind(select(bac_meta, closest_placement_reference, species) %>% rename(accession = closest_placement_reference) ) %>%
+  filter(!is.na(species)) %>% unique
+
+other_ref <- bac_meta$other_related_references.genome_id.species_name.radius.ANI.AF. %>% unique %>%
+  str_split("; ") %>% lapply(function(x) x[1:2]) %>% lapply(function(x) data.frame(accession = x[1], species = x[2])) %>% bind_rows() %>%
+  mutate(species = str_remove(species, "s__")) %>% filter(!is.na(species)) %>% unique
+
+ref_info <- rbind(ref_info, other_ref) %>% unique
+
+write.csv(ref_info, file = file.path(subdir, "reference_genomes_info.csv"), row.names = FALSE, quote = FALSE)
 
 # Now extract clades with reference genomes from GTDBtk tree
-cod_tips_full <- data.frame(node = character(), tip = character(), is.mag = logical(), mag_label = character(), stringsAsFactors = FALSE)
+cod_tips_full <- data.frame(node = character(), tip = character(), is.mag = logical(), label = character(), stringsAsFactors = FALSE)
+
+forward_branches <- list("Propionibacterium" = "host_order=='Artiodactyla'",
+                         "Actinomyces.6" = "host_order=='Artiodactyla'",
+                         "Corynebacterium.8" = "host_order=='Carnivora'")
 
 for (n in unique(cod_tips_df$node)) {
+  print(n)
   # Get bins from that node
   bins <- cod_tips_df %>% filter(node == n) %>% pull(bin) %>% str_remove(".gz")
   # Extract the corresponding clade from the full tree including reference genomes
@@ -78,13 +97,32 @@ for (n in unique(cod_tips_df$node)) {
   tips <- getDescendants(bac_tree_full, node)
   tips <- tips[tips <= length(bac_tree_full$tip.label)]
   tip_names <- bac_tree_full$tip.label[tips] %>% str_remove("GB_|RS_")
-  df <- data.frame(node = n, tip = tip_names, is.mag = tip_names %in% bins)
+  df <- data.frame(node = n, tip = tip_names, is.mag = grepl("MEGAHIT", tip_names))
   df$tip <- ifelse(df$is.mag, paste0(df$tip, ".gz"), df$tip)
-  df$mag_label <- cod_tips_df$tip[match(df$tip, cod_tips_df$bin, ".gz")]
+  # Add metadata
+  df <- df %>% left_join(cod_tips_df, by = c("node" = "node", "tip" = "bin")) %>%
+    rename(label = tip.y) %>%
+    # Keep only dereplicated MAGs (i.e. those that are in the tree)
+    filter(!is.na(label) | !is.mag)
   # Make sure to keep only HQ bins
-  df$Completeness <- cod_tips_df$Completeness[match(df$tip, cod_tips_df$bin, ".gz")]
-  df$Contamination <- cod_tips_df$Contamination[match(df$tip, cod_tips_df$bin, ".gz")]
   df <- df %>% filter((Completeness >= 90 & Contamination <= 5) | !is.mag )
+  # Add reference genome labels
+  df$label <- ifelse(df$is.mag, df$label,
+                     ref_info$species[match(df$tip, ref_info$accession)])
+  df$forward_branch <- NA
+  # Define forward branches when relevant
+  if (n %in% names(forward_branches)) {
+    fg_tips <- cod_tips_df %>% filter(node == n) %>%
+          filter(!!parse_expr((forward_branches[[n]]))) %>% pull(bin) %>% str_remove(".gz")
+    fg_node <- getMRCA(bac_tree_full, fg_tips)
+    fg_tips_all <- getDescendants(bac_tree_full, fg_node)
+    fg_tips_all <- fg_tips_all[fg_tips_all <= length(bac_tree_full$tip.label)]
+    fg_tip_names <- bac_tree_full$tip.label[fg_tips_all] %>% str_remove("GB_|RS_")
+    df <- df %>% mutate(forward_branch = case_when(
+      str_remove(tip, ".gz") %in% fg_tip_names ~ "#1",
+      TRUE ~ "#0"
+    ))
+  }
   df <- df %>% select(-c(Completeness, Contamination))
   write.csv(df, file = file.path(subdir, paste0("clade_", n, "_with_refs.csv")), row.names = FALSE, quote = FALSE)
   cod_tips_full <- rbind(cod_tips_full, df)
